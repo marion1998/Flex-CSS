25ff9037fa36802a597d5a0f2618eb21338bf2b4
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-05-21 17:56:43 +0200

    grid column

--- scripts/scriptgrid.js
+++ scripts/scriptgrid.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
         var btnName = $(this).attr('name');
         var divExample = $(this).parents('.codeblock').siblings('.example');
 
-         if (btnName == 'grid-row-start' || btnName == 'grid-row-end' || btnName == 'grid-row'){
+         if (btnName == 'grid-row-start' || btnName == 'grid-row-end' || btnName == 'grid-row'
+         || btnName == 'grid-column-start' || btnName == 'grid-column-end' || btnName == 'grid-column' ){
             divExample.children(".example-grid-item").css(btnName, btnText);
 
         divExample.css(btnName, btnText);
